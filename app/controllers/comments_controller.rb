b3a694cc8e1bc13d1216d61379d4df9bd3ad9dd4
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-05-11 16:49:07 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+    @comment.user.build
+  end
+
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post
@@ -8,6 +13,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username])
   end
 end
